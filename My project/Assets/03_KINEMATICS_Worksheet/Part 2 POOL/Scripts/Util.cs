60411c6c4e6022799cc22b1fc2c7f2731d01d6fd
Lychesann <zukhairyfaris2003@gmail.com>
2023-11-27 19:24:55 +0800

    Just did some codes from the worksheet

--- My project/Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Util.cs
+++ My project/Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Util.cs
@@ -1,12 +1,16 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class Util
-// {
-//     public static float FindDistance(HVector2D p1, HVector2D p2)
-//     {
-//         return /*your code here*/;
-//     }
-// }
+public class Util
+{
+    public static float FindDistance(HVector2D p1, HVector2D p2)
+    {
+        float px = p1.x - p2.x;
+        float py = p1.y - p2.y;
+        float mag = px * px + py * py;
+        return Mathf.Sqrt(mag);
+    }
+}
 
