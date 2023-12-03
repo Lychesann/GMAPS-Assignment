b0fbf4f55ed9e8a3ce1376092514c3d33f6bf0e1
Lychesann <zukhairyfaris2003@gmail.com>
2023-12-03 11:47:42 +0800

    Updated the math for util.cs

--- My project/Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Util.cs
+++ My project/Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Util.cs
@@ -7,7 +7,7 @@ public class Util
 {
     public static float FindDistance(HVector2D p1, HVector2D p2)
     {
-        return Mathf.Sqrt(p2.x *2 - p1.x * 2 + p2.y *2 + p1.y*2);
+        return Mathf.Sqrt(p2.x * p1.x  + p2.y  * p1.y);
     }
 }
 
